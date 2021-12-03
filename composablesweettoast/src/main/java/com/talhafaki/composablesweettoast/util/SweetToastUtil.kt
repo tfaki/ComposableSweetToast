556b339e702e3368351771947769f6fef71d3c02
talhafaki <talhafaki05@gmail.com>
2021-12-03 10:00:49 +0300

    refactor

--- composablesweettoast/src/main/java/com/talhafaki/composablesweettoast/util/SweetToastUtil.kt
+++ composablesweettoast/src/main/java/com/talhafaki/composablesweettoast/util/SweetToastUtil.kt
@@ -1,7 +1,6 @@
 package com.talhafaki.composablesweettoast.util
 
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -12,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -27,43 +27,31 @@ import com.talhafaki.composablesweettoast.toasttypes.Warning
 object SweetToastUtil {
 
     @Composable
-    fun SweetSuccess(message: String) {
-        val sweetToast = SweetToast.MakeTest(
-            message,
-            duration = Toast.LENGTH_LONG,
-            type = Success()
-        )
-        sweetToast.show()
+    fun SweetSuccess(message: String, duration: Int = Toast.LENGTH_LONG) {
+        val sweetSuccessToast = SweetToast(LocalContext.current)
+        sweetSuccessToast.MakeTest(message = message, duration = duration, type = Success())
+        sweetSuccessToast.show()
     }
 
     @Composable
-    fun SweetError(message: String) {
-        val sweetToast = SweetToast.MakeTest(
-            message,
-            duration = Toast.LENGTH_LONG,
-            type = Error()
-        )
-        sweetToast.show()
+    fun SweetError(message: String, duration: Int = Toast.LENGTH_LONG) {
+        val sweetErrorToast = SweetToast(LocalContext.current)
+        sweetErrorToast.MakeTest(message = message, duration = duration, type = Error())
+        sweetErrorToast.show()
     }
 
     @Composable
-    fun SweetInfo(message: String) {
-        val sweetToast = SweetToast.MakeTest(
-            message,
-            duration = Toast.LENGTH_LONG,
-            type = Info()
-        )
-        sweetToast.show()
+    fun SweetInfo(message: String, duration: Int = Toast.LENGTH_LONG) {
+        val sweetInfoToast = SweetToast(LocalContext.current)
+        sweetInfoToast.MakeTest(message = message, duration = duration, type = Info())
+        sweetInfoToast.show()
     }
 
     @Composable
-    fun SweetWarning(message: String) {
-        val sweetToast = SweetToast.MakeTest(
-            message,
-            duration = Toast.LENGTH_LONG,
-            type = Warning()
-        )
-        sweetToast.show()
+    fun SweetWarning(message: String, duration: Int = Toast.LENGTH_LONG) {
+        val sweetWarningToast = SweetToast(LocalContext.current)
+        sweetWarningToast.MakeTest(message = message, duration = duration, type = Warning())
+        sweetWarningToast.show()
     }
 
     @Composable
