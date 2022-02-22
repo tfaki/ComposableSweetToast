6f4a00c54d84137448044fb5ce5a9d263e12317a
talhafaki <talhafaki05@gmail.com>
2022-02-22 23:16:11 +0300

    add contentAlignment and padding, upgrade dependencies

--- composablesweettoast/src/main/java/com/talhafaki/composablesweettoast/util/SweetToastUtil.kt
+++ composablesweettoast/src/main/java/com/talhafaki/composablesweettoast/util/SweetToastUtil.kt
@@ -27,60 +27,121 @@ import com.talhafaki.composablesweettoast.toasttypes.Warning
 object SweetToastUtil {
 
     @Composable
-    fun SweetSuccess(message: String, duration: Int = Toast.LENGTH_LONG) {
+    fun SweetSuccess(
+        message: String,
+        duration: Int = Toast.LENGTH_LONG,
+        padding: PaddingValues = PaddingValues(0.dp),
+        contentAlignment: Alignment = Alignment.BottomCenter
+    ) {
         val sweetSuccessToast = SweetToast(LocalContext.current)
-        sweetSuccessToast.MakeTest(message = message, duration = duration, type = Success())
+        sweetSuccessToast.MakeTest(
+            message = message,
+            duration = duration,
+            type = Success(),
+            padding = padding,
+            contentAlignment = contentAlignment
+        )
         sweetSuccessToast.show()
     }
 
     @Composable
-    fun SweetError(message: String, duration: Int = Toast.LENGTH_LONG) {
+    fun SweetError(
+        message: String,
+        duration: Int = Toast.LENGTH_LONG,
+        padding: PaddingValues = PaddingValues(0.dp),
+        contentAlignment: Alignment = Alignment.BottomCenter
+    ) {
         val sweetErrorToast = SweetToast(LocalContext.current)
-        sweetErrorToast.MakeTest(message = message, duration = duration, type = Error())
+        sweetErrorToast.MakeTest(
+            message = message,
+            duration = duration,
+            type = Error(),
+            padding = padding,
+            contentAlignment = contentAlignment
+        )
         sweetErrorToast.show()
     }
 
     @Composable
-    fun SweetInfo(message: String, duration: Int = Toast.LENGTH_LONG) {
+    fun SweetInfo(
+        message: String,
+        duration: Int = Toast.LENGTH_LONG,
+        padding: PaddingValues = PaddingValues(0.dp),
+        contentAlignment: Alignment = Alignment.BottomCenter
+    ) {
         val sweetInfoToast = SweetToast(LocalContext.current)
-        sweetInfoToast.MakeTest(message = message, duration = duration, type = Info())
+        sweetInfoToast.MakeTest(
+            message = message,
+            duration = duration,
+            type = Info(),
+            padding = padding,
+            contentAlignment = contentAlignment
+        )
         sweetInfoToast.show()
     }
 
     @Composable
-    fun SweetWarning(message: String, duration: Int = Toast.LENGTH_LONG) {
+    fun SweetWarning(
+        message: String,
+        duration: Int = Toast.LENGTH_LONG,
+        padding: PaddingValues = PaddingValues(0.dp),
+        contentAlignment: Alignment = Alignment.BottomCenter
+    ) {
         val sweetWarningToast = SweetToast(LocalContext.current)
-        sweetWarningToast.MakeTest(message = message, duration = duration, type = Warning())
+        sweetWarningToast.MakeTest(
+            message = message,
+            duration = duration,
+            type = Warning(),
+            padding = padding,
+            contentAlignment = contentAlignment
+        )
         sweetWarningToast.show()
     }
 
     @Composable
-    fun SetView(messageTxt: String, resourceIcon: Int, backgroundColor: Color) {
-        Surface(modifier = Modifier.wrapContentSize(), color = Color.Transparent) {
-            Row(
+    fun SetView(
+        messageTxt: String,
+        resourceIcon: Int,
+        backgroundColor: Color,
+        padding: PaddingValues,
+        contentAlignment: Alignment
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            contentAlignment = contentAlignment
+        ) {
+            Surface(
                 modifier = Modifier
-                    .background(
-                        color = backgroundColor,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .wrapContentWidth()
-                    .wrapContentHeight()
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                    .wrapContentSize(),
+                color = Color.Transparent
             ) {
-                Image(
-                    modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp),
-                    painter = painterResource(id = resourceIcon),
-                    contentDescription = "",
-                )
-                Text(
-                    text = messageTxt,
-                    textAlign = TextAlign.Center,
-                    fontSize = 16.sp,
-                    style = TextStyle(color = whiteBackground),
-                    modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp)
-                )
+                Row(
+                    modifier = Modifier
+                        .background(
+                            color = backgroundColor,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .wrapContentWidth()
+                        .wrapContentHeight()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp),
+                        painter = painterResource(id = resourceIcon),
+                        contentDescription = "",
+                    )
+                    Text(
+                        text = messageTxt,
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp,
+                        style = TextStyle(color = whiteBackground),
+                        modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp)
+                    )
+                }
             }
         }
     }
